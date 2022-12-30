4e61f9675191d22a5d359c5fa4fe9f19688087c8
unknown <loknathsds66@gmail.com>
2022-12-30 22:07:24 +0600

    dependency install

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello Bangladesh</h1>
+     <h1 className='bg-red-500'>Hello Bangladesh</h1>
     </div>
   );
 }
