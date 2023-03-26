a3abd82e9a8773f96b136ed01299228f515a55b5
unknown <loknathsds66@gmail.com>
2023-03-26 11:54:40 +0600

    login page with responsived done

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+    return (
+        <section className="bg-gray-50">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <div className="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                            Sign in to your account
+                        </h1>
+                        <form className="space-y-4 md:space-y-6" action="#">
+                            <div>
+                                <label for="email" className="block mb-2 text-sm font-medium text-gray-900 ">Your email</label>
+                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" required=""/>
+                            </div>
+                            <div>
+                                <label for="password" className="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
+                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required=""/>
+                            </div>
+                            <div className="flex items-center justify-between">
+                                <div className="flex items-start">
+                                    <div className="flex items-center h-5">
+                                        <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 " required=""/>
+                                    </div>
+                                    <div className="ml-3 text-sm">
+                                        <label for="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
+                                    </div>
+                                </div>
+                                <p  className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</p>
+                            </div>
+                            <button type="submit" className="w-full bg-blue-700 text-white  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
+                            <p className="text-lg font-semibold text-gray-500 ">
+                                Don’t have an account yet? <Link to="/register" className="font-medium text-blue-700 hover:underline">Sign up</Link>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Login;
--- src/utils/Routing/Routing.jsx
+++ src/utils/Routing/Routing.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Checkout from '../../pages/Checkout/Checkout';
 import Home from '../../pages/Home/Home';
+import Login from '../../pages/Login/Login';
 import ProductDetail from '../../pages/ProductDetail/ProductDetail';
 import Products from '../../pages/Products/Products';
+import Register from '../../pages/Register/Register';
 import Success from '../../pages/Success/Success';
 
 const Routing = () => {
@@ -14,6 +16,8 @@ const Routing = () => {
             <Route path="/product-detail/:id" element={<ProductDetail />} />
             <Route path='/checkout' element={<Checkout />} />
             <Route path='/success' element={<Success />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register/>} />
             <Route path='dashboard' element={''} />
         </Routes>
     );
