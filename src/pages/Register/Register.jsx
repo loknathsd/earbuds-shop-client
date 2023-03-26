fd57b02edd6de6f40fe86d50dbe20a1b641c0cba
unknown <loknathsds66@gmail.com>
2023-03-26 12:07:37 +0600

    register page done

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            Register
-        </div>
+        <section className="bg-gray-50">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                            Create and account
+                        </h1>
+                        <form className="space-y-4 md:space-y-6" action="#">
+                            <div>
+                                <label for="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
+                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5" placeholder="name@company.com" required="" />
+                            </div>
+                            <div>
+                                <label for="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5" required="" />
+                            </div>
+                            <div>
+                                <label for="confirm-password" className="block mb-2 text-sm font-medium text-gray-900">Confirm password</label>
+                                <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 " required="" />
+                            </div>
+                            <div className="flex items-start">
+                                <div className="flex items-center h-5">
+                                    <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300" required="" />
+                                </div>
+                                <div className="ml-3 text-sm">
+                                    <label for="terms" className=" text-gray-500 dark:text-gray-300">I accept the <span className="font-medium text-blue-600 hover:underline " >Terms and Conditions</span></label>
+                                </div>
+                            </div>
+                            <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Create an account</button>
+                            <p className="text-sm  text-gray-500">
+                                Already have an account? <Link to="/login" className="font-medium text-blue-600 hover:underline">Login here</Link>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
